9fa9ec448ac4d4b65ae735929e8cc2cd647e77c6
isaclee <ilee29@jhmi.edu>
2019-06-13 00:31:42 -0400

    start deseq2

--- analysis/chickenRNAseq_deseq2.R
+++ analysis/chickenRNAseq_deseq2.R
@@ -1,7 +1,8 @@
 #!/usr/bin/env Rscript
 library(tidyverse)
+library(DESeq2)
 
-# prepare data ####
+# prepare data ----
 root="/mithril/Data/NGS/projects/chicken/rnaseq"
 names.fp = "/mithril/Data/NGS/projects/chicken/select_genes_ensembl.txt"
 qdir=file.path(root,"quants")
@@ -9,15 +10,16 @@ qdir=file.path(root,"quants")
 dirs = system(paste("find",qdir,"-maxdepth 1 -type d -name \"E*\""),intern=T)
 samps = sapply(strsplit(basename(dirs),"_"),"[[",1)
 ages = sapply(strsplit(samps,"r|c"),"[[",1)
+groups = substr(samps,1,nchar(samps)-1)
 cells_tmp = sapply(strsplit(samps,"6|8"),"[[",2)
 cells = sapply(strsplit(cells_tmp,"1|2"),"[[",1)
 reps = as.numeric(sapply(strsplit(cells_tmp,"a"),"[[",2))
-pd = tibble(sample = samps, age = ages,
+pd = tibble(sample = samps, group = groups,age = ages,
             cell = cells, rep = reps,
             dir = dirs,
             filepath = file.path(dir,"quant.sf"))
 
-# read in data ####
+# read in data ----
 dat.list = lapply(seq_along(pd$filepath),function(i){
   read_tsv(pd$filepath[i]) %>%
     mutate(sample = pd$sample[i],
@@ -30,13 +32,14 @@ dat.all = do.call(rbind,dat.list)
 # strip version in names
 dat.all$ensembl = sapply(strsplit(dat.all$Name,"[.]"),"[[",1)
 
-# reorganize data for deseq2 ####
+# reorganize data for deseq2 ----
 dat.spread = dat.all %>% 
   select(ensembl,TPM,sample) %>%
   spread(sample,TPM)
+dat.mat <- dat.spread[,-1]
 pd = pd[match(colnames(dat.spread)[-1],pd$sample),]
 
-# subset genes for test ####
+# subset genes for test ----
 cnames = c("ensembl","gene")
 genes = read_tsv(names.fp,col_names=cnames)
 dat.select = dat.spread[match(genes$ensembl,dat.spread$ensembl),] %>%
